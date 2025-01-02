52ea3865628adaa16bc614cc47bc836418467807
Logan Kulinski <rashes_lineage02@icloud.com>
2025-01-02 13:06:52 -0600

    Bus APIs

--- src/main/java/app/cta4j/model/ArrivalBody.java
+++ src/main/java/app/cta4j/model/ArrivalBody.java
@@ -5,8 +5,12 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Builder;
 
 import java.util.List;
+import java.util.Objects;
 
 @Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record ArrivalBody<T extends Arrival>(@JsonAlias(value = {"eta", "prd"}) List<T> arrivals) {
+    public ArrivalBody {
+        Objects.requireNonNull(arrivals);
+    }
 }
--- src/main/java/app/cta4j/model/ArrivalResponse.java
+++ src/main/java/app/cta4j/model/ArrivalResponse.java
@@ -3,9 +3,14 @@ package app.cta4j.model;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import lombok.Builder;
 
+import java.util.Objects;
+
 @Builder
 public record ArrivalResponse<T extends Arrival>(
     @JsonAlias(value = {"ctatt", "bustime-response"})
     ArrivalBody<T> body
 ) {
+    public ArrivalResponse {
+        Objects.requireNonNull(body);
+    }
 }
