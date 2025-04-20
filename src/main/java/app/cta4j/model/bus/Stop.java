864d0465d0d10caa0819bcafc3b37e376c4742ba
Logan Kulinski <rashes_lineage02@icloud.com>
2025-04-20 14:55:51 -0500

    DynamoDB migration

--- src/main/java/app/cta4j/model/bus/Stop.java
+++ src/main/java/app/cta4j/model/bus/Stop.java
@@ -2,31 +2,19 @@ package app.cta4j.model.bus;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 import lombok.Builder;
+import lombok.Value;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbImmutable;
 
 import java.math.BigDecimal;
 import java.util.Objects;
 
+@Value
 @Builder
-public record Stop(
+@DynamoDbImmutable(builder = Stop.StopBuilder.class)
+public class Stop {
     @JsonAlias("stpid")
-    String id,
+    String id;
 
     @JsonAlias("stpnm")
-    String name,
-
-    @JsonAlias("lat")
-    BigDecimal latitude,
-
-    @JsonAlias("lon")
-    BigDecimal longitude
-) {
-    public Stop {
-        Objects.requireNonNull(id);
-
-        Objects.requireNonNull(name);
-
-        Objects.requireNonNull(latitude);
-
-        Objects.requireNonNull(longitude);
-    }
+    String name;
 }
